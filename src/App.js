4ed53e0d22f7fea85c17a6c26a21ed4a45c4e41f
Muhammed Hassan <m.hassananwer@gmail.com>
2022-09-08 07:59:54 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { ChakraProvider, Box, theme, VStack, Text } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const getCounter = setInterval(() => {
+      setCounter(prev => (prev <= 360 ? prev + 1 : 0));
+    }, 25);
+
+    return () => {
+      clearInterval(getCounter);
+    };
+  }, [counter]);
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+      <Box
+        textAlign="center"
+        h="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDir="column"
+      >
+        <ColorModeSwitcher alignSelf="center" justifySelf="center" />
+        <VStack
+          fontFamily="Public Sans"
+          bgGradient={`linear(to-l, hsl(${270 + counter}, 67%, 47%), hsl(${
+            330 + counter
+          }, 100%, 50%))`}
+          bgClip="text"
+        >
+          <Text fontSize={['5xl', '5xl', '9xl']}>
+            {new Date().toLocaleTimeString()}
+          </Text>
+          <Text fontSize={['md', 'md', '2xl']}>
+            {new Date().toLocaleDateString()}
+          </Text>
+        </VStack>
       </Box>
     </ChakraProvider>
   );
